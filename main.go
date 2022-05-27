9701743f99052c9bff027829b95267ae4cde2008
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2022-05-27 21:48:46 +0600

    imported gjson

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	//"encoding/json"
+	"floatChecker/requests"
+	_"fmt"
+	_"io"
+	_ "math"
+	_ "strings"
+)
 
 func main() {
+	url := "https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20AK-47%20%7C%20Uncharted%20%28Field-Tested%29/render/?query=country=EU&language=english&currency=1"
+	requests.SearchCurrentItem(url)
 
 }
--- requests/parser.go
+++ requests/parser.go
@@ -1,2 +1,37 @@
 package requests
 
+import (
+	"fmt"
+	"io"
+	_ "io"
+	_ "math"
+	"net/http"
+	_ "strings"
+	_"time"
+
+	"github.com/tidwall/gjson"
+)
+
+func SearchCurrentItem(url string) {
+
+	myClient := &http.Client{}
+	res, err := myClient.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	data := gjson.Get(string(body), "listinginfo")
+	gjson.ForEachLine(data.String(), func(line gjson.Result) bool {
+		fmt.Println(line.String())
+		/* fmt.Println(gjson.Get(string(line.Raw), "listingid")) */
+		return true
+	})
+	fmt.Println(data)
+	defer res.Body.Close()
+}
