9f90b915801fd88dca5c9a97362a422eab594b89
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2022-05-31 00:15:09 +0600

    ass

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ import (
 func main() {
 	url := "https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20AK-47%20%7C%20Uncharted%20%28Field-Tested%29/render/?query=country=EU&language=english&currency=1"
 	itemsList := requests.SearchCurrentItem(url)
-	fmt.Println(len(itemsList))
+	fmt.Println("Elements: ", len(itemsList))
 }
