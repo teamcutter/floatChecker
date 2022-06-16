bf9a0753554855dbb66e271e8473573645de79e2
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2022-06-17 01:52:08 +0600

    rewrited to GIN

--- main.go
+++ main.go
@@ -4,21 +4,21 @@ import (
 	"floatChecker/internal/requests"
 	"net/http"
 
-	"github.com/labstack/echo/v4"
+	"github.com/gin-gonic/gin"
 )
 
 const thousand = "https://steamcommunity.com/market/listings/730/AK-47%20%7C%20Slate%20%28Minimal%20Wear%29/render/?query=country=EU&language=english&currency=1"
 // тут много https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20AK-47%20%7C%20Uncharted%20%28Field-Tested%29/render/?query=country=EU&language=english&currency=1
 
 func main() {
-	app := echo.New()
+	app := gin.Default()
 	url := "https://steamcommunity.com/market/listings/730/StatTrak™%20Desert%20Eagle%20%7C%20Directive%20%28Field-Tested%29/render/?query=country=EU&language=english&currency=1"
 	
-	app.GET("/info", func(ctx echo.Context) error {
+	app.GET("/info", func(c *gin.Context) {
 		links := requests.SearchCurrentItem(url)
 		floatInfoList := requests.InfoCurrentItem(links)
-		return ctx.JSON(http.StatusOK, floatInfoList)
+		c.IndentedJSON(http.StatusOK, floatInfoList)
 	})
 
-	app.Logger.Fatal(app.Start(":1323"))
+	app.Run(":8080")
 }
